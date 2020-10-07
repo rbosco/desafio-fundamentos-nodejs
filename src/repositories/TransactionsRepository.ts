93e97b2eab8cd475232b2f78ee4cde5149b57359
compartilhaclub_ti <compartilhaclub.ti@gmail.com>
2020-10-07 20:40:47 -0300

    :S parkles: challenge finalized

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionOTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,45 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    const {income,outcome}  = this.transactions.reduce((acumulator:Balance,transaction:Transaction) => {
+      switch(transaction.type){
+        case"income":
+          acumulator.income+=transaction.value;
+          break;
+        case"outcome":
+          acumulator.outcome+=transaction.value;
+          break;
+        default:
+        break;
+      }
+      return acumulator;
+    },{
+      income:0,
+      outcome:0,
+      total:0
+    });
+
+    const total = income - outcome;
+
+    return {income,outcome,total};
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTransactionOTO): Transaction {
     // TODO
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
